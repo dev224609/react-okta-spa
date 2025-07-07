a92f67d986dc5e4c87bbb6d933bb739209816a72
dev224609 <74307594+dev224609@users.noreply.github.com>
2025-07-07 17:40:13 +0530

    Fixed navbar.js warnings - 01

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Security, } from '@okta/okta-react';
 
 import App from './App';
 import { oktaAuth } from './oktaConfig';
